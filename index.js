69c967f41b3184f8bbc8d9e6a0863a8d6925cc9f
Emma Nyakio Gacoki <emmanyakio@gmail.com>
2022-06-12 02:30:51 +0300

    commit tree 4716ca35ae48a33d04d00d9b0e602c0179d0f3ea

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase();
+}
+//console.log(upperCaseCustomerName());
+function setBestCustomer(){
+  bestCustomer = "not bob";
+}
+//console.log(setBestCustomer())
+
+function overwriteBestCustomer(){
+  bestCustomer = "maybe bob";
+}
+//console.log(overwriteBestCustomer())
+const leastFavoriteCustomer = "Jane";
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = "Brian";
+
+}
+//console.log(changeLeastFavoriteCustomer())
+
